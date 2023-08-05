ef38a11472486889cf06bff172a874c2129cff78
soni895 <darshansoni895@gmail.com>
2023-08-05 19:20:41 +0530

    local file upload successful

--- controllers/fileupload.js
+++ controllers/fileupload.js
@@ -7,7 +7,8 @@ exports.localfileupload=(req,res)=>
         const file= req.files.file;
         console.log(file);
 
-        const path=__dirname+"/files/"+Date.now();
+        const path=__dirname+"/files/"+Date.now()+`.${file.name.split(".")[1]}`;
+
      
         console.log(`file path => ${path} and current path => ${__dirname} and file name ${__filename} `);
         file.mv(path,(error)=>
