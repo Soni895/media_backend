4d2c7eb852a37066e0357359d54f43f315302d06
soni895 <darshansoni895@gmail.com>
2023-08-05 15:58:49 +0530

    cloundinary setup completed

--- config/coloudinary.js
+++ config/coloudinary.js
@@ -1,1 +1,20 @@
- 
+const cloudinary = require('cloudinary').v2
+require.apply("dotenv").config();
+const  api_secret=process.env.api_secret;
+const  api_key=process.env.api_key;
+const cloud_name=process.env.cloud_name;
+
+exports.cloudnary()
+{
+    try {
+        cloudinary.config(
+            {
+
+            }
+
+        );
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
