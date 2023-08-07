b156b389751774f589095a3f8f8bc36f337aff6f
soni895 <darshansoni895@gmail.com>
2023-08-07 22:03:32 +0530

    node mailer completed

--- models/file.js
+++ models/file.js
@@ -1,5 +1,6 @@
 const mongoose=require("mongoose");
 const nodemailer = require('nodemailer');
+require("dotenv").config();
 const fileschema=new mongoose.Schema(
     {
         name:
@@ -28,8 +29,31 @@ const fileschema=new mongoose.Schema(
         {
             try {
                 console.log(doc);
+                const mail_host=process.env.mail_host;
+                const mail_user=process.env.mail_user;
+                const mail_pass=process.env.mail_pass;
+
+                console.log(mail_host,mail_pass,mail_user);
+
 
                 // create transporter
+
+                const transporter=nodemailer.createTransport(
+                    {
+                        host: mail_host,
+                        auth: {
+                          user: mail_user,
+                          pass: mail_pass,
+                    }
+                });
+                const mailOptions = {
+                    from:`darshan soni`,
+                    to: doc.email,
+                    subject: "email testing",
+                    html:`<img src=${doc.imgurl}>` 
+                  };
+                const info= await transporter.sendMail(mailOptions);
+                console.log(info);
                 
             } catch (error) {
                 console.log(error);
