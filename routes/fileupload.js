4516b52e167a8684fb7806bb1b313f9830bcbe3c
soni895 <darshansoni895@gmail.com>
2023-08-17 12:44:10 +0530

    code change

--- routes/fileupload.js
+++ routes/fileupload.js
@@ -1,7 +1,9 @@
 const express =require("express");
 const router=express.Router();
 const {localfileupload,videoupload,imgupload}=require("../controllers/fileupload");
-const  {imgsizereducer} = require("../controllers/fileupload");
+const {imgsizereducer}= require("../controllers/fileupload");
+
+
 
 router.post("/localfileupload",localfileupload);
 router.post("/imgupload",imgupload);
